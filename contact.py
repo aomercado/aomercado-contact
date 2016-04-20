7c146271d3911b8ddb2aae001e13f70d369509e7
Matheus Pereira <matheper@gmail.com>
2016-04-20 09:58:21 -0300

    Added market s contact method.

--- contact.py
+++ contact.py
@@ -4,9 +4,12 @@ from flask import jsonify
 from flask import request
 from flask.ext.cors import CORS
 from flask.ext.pymongo import PyMongo
-from flask.ext.pymongo import ASCENDING
 
-from localsettings import *
+from localsettings import DBNAME
+from localsettings import HOST
+from localsettings import PORT
+from localsettings import USERNAME
+from localsettings import PASSWORD
 
 
 app = Flask(__name__)
@@ -20,6 +23,9 @@ mongo = PyMongo(app, config_prefix='MONGO')
 
 CORS(app, resources={
     r"/contact/*": {
+        "origins": [r".*aomercado\.github\.io", r".*aomercado\.com"]
+    },
+    r"/market/*": {
         "origins": [r".*aomercado\.github\.io", r".*aomercado\.com"]
     }
 })
@@ -43,6 +49,33 @@ def get_mail(mail):
     if not contact:
         abort(404)
     return jsonify({'contact': make_public(contact)})
+
+
+@app.route('/market', methods=['POST'])
+def receive_market():
+    if not request.json or 'mail' not in request.json or \
+       'name' not in request.json or 'phone' not in request.json or \
+       'market' not in request.json or 'message' not in request.json:
+        abort(400)
+    message = {
+        'mail': request.json['mail'],
+        'name': request.json['name'],
+        'phone': request.json['phone'],
+        'market': request.json['market'],
+        'message': request.json['message'],
+    }
+    market_contacts = mongo.db.get_collection('marketcontacts')
+    market_contacts.insert_one(message)
+    return jsonify({'message': make_public(message)}), 201
+
+
+@app.route('/market/<mail>', methods=['GET'])
+def get_message(mail):
+    market_contacts = mongo.db.get_collection('marketcontacts')
+    message = market_contacts.find_one({'mail': mail})
+    if not message:
+        abort(404)
+    return jsonify({'message': make_public(message)})
 
 
 def make_public(contact):
